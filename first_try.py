1b045dc394d8d7a1580ea12213a548d11da17d35
Lucrethon <lucrecimich@gmail.com>
2025-10-14 20:47:05 -0500

    set clock

--- first_try.py
+++ first_try.py
@@ -8,13 +8,19 @@ pygame.init()
 screen = pygame.display.set_mode((1600, 960))
 
 #Set player using gif-pygame library
-player = gif_pygame.load("./assets/Personaje_Arnaldo_pequeño_escalado.gif").convert()
+player = gif_pygame.load("./assets/Personaje_Arnaldo_pequeño_escalado.gif")
 
 #Load background image
 background = pygame.image.load("./assets/Fondo.png").convert()
 
-# Resize image to fit window
+# Resize background image to fit window
 background = pygame.transform.scale(background, (1600, 960))
+
+#set game clock to control the time the loop
+clock = pygame.time.Clock() 
+#Each round loop is a frame 
+#without the control (clock), the loop will run at the fastest speed that the computer can allow 
+
 
 
 #convert()=
@@ -35,14 +41,28 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
         # pygame.K_SCAPE event means the user press Esc button to close your window
+    
+    #set the frames per second 
+    fps = clock.tick(60)  # limita el juego a 60 FPS
+    #clock.tick returns the miliseconds since the last frame
+    
+    #delta time 
+    delta_time = fps / 1000
+    #we divide it between 1000 to tranform into seconds 
+    
+    player_speed += 200 * delta_time
+    
+    #if speed = 200 and delta_time = 0.016 (1/60 fps), the player is moving at 200 * 0.016 = 3.2 píxels in that frame
+
 
     #Set background image
     screen.blit(background, (0, 0))
     
-    #set player using gif-pygame library
-    screen.blit(player, (x, 0))
+    #set player using gif-pygame library in initial position
+    player.render(screen, (0, 510))
 
-            
+
+    #update screen            
     pygame.display.flip()
             
 pygame.quit()
